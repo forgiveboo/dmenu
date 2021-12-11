4b8b1ebff502469c89db3614fc3224867173aeba
forgiveboo <wyx5051@gmail.com>
2021-12-11 17:43:25 +0800

    add alpha

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xf0;
+static const unsigned int alpha = 0xd0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Source Code Pro:size=16"
@@ -32,4 +32,4 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-static unsigned int border_width = 2;
+static unsigned int border_width = 0;
